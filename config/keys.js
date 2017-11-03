172ebd2a26863b4f2788577e3e8a8704abc68f91
Derek Alia <derekalia@me.com>
2017-11-03 04:59:55 -0700

    removed files

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleClientID: '461433562493-mkhj10vhvme9rkfh61p8eqp27eg7brlh.apps.googleusercontent.com',
   googleClientSecret: '7dc0NybiLXqxb-s7GYAAWKOM',
-  mongoURI: 'mongodb://derek:abuh12@ds127105.mlab.com:27105/blockhead-db'
+  mongoURI: 'mongodb://derek:abuh12@ds127105.mlab.com:27105/blockhead-db',
+  cookieKey: 'aslkdjeasidoj'
 };
