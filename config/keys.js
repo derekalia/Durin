4c4cd09587cf02758ecc75c77f356effdb8ce48c
Derek Alia <derekalia@me.com>
2017-11-03 19:01:19 -0700

    fixed keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-if(process.env.NODE_ENV==='production'){
+if (process.env.NODE_ENV === 'production') {
   //we are in production
-  module.exports = require('./prod')
-} else{
+  module.exports = require('./prod.js');
+} else {
   //we are in develop - return key
-  module.exports = require('./dev')
+  module.exports = require('./dev.js');
 }
